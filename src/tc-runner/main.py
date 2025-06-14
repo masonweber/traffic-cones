c45c2d8e058baff43b218f438809911831cb2bc9
Mason Weber <mason.weber.cpe@gmail.com>
2025-06-14 01:34:41 -0400

    main comments and cones.txt updated project description

--- src/tc-runner/main.py
+++ src/tc-runner/main.py
@@ -2,16 +2,18 @@ import time
 from threading import Thread
 
 def main():
-    t = Thread(target=cust_one)
-    t.run()
+#    t = Thread(target=cust_one)
+#    t.run()
 #    while (1==1):
 #        print(time.monotonic())
 #        time.sleep(0.9)
 #        while (time.monotonic() % 1 > 0.01):
 #            time.sleep(0.001)
+    print("Welcome to the DMV. Take a number and a seat and we'll be right with you.")
 
-def cust_one():
-    print("thread safe")
+
+#def cust_one():
+#    print("thread safe")
 
 if __name__ == '__main__':
     main()
